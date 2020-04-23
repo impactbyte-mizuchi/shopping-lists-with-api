bc259aa4ffddebe94a1fd81f0a42ef57e0df1ad8
erfan <vanoer21@gmail.com>
2020-04-23 16:19:19 +0700

    showshoppinglist

--- src/components/pages/ShoppingListsPage.js
+++ src/components/pages/ShoppingListsPage.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Formik, Form, } from 'formik';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
 
 function ShoppingListsPage() {
+   const [allList, setAllList] = useState([])
+   useEffect(function () {
+      const user = JSON.parse(localStorage.getItem('userLogin'));
+      const url = `${process.env.REACT_APP_MOCKAPI_URL}/users/${user.id}/shopping-lists`;
+      fetch(url)
+         .then(
+            response => response.json()
+         )
+         .then(
+            results => {
+               console.log(results);
+
+               setAllList(results)
+            }
+         )
+         .catch(
+            error => console.log(error)
+         )
+   }, [])
+   console.log();
+
    return (
       <div style={{ textAlign: 'center' }}>
          <h1>ShoppingList Page</h1>
@@ -30,7 +58,7 @@ function ShoppingListsPage() {
                console.log(values)
                const user = JSON.parse(localStorage.getItem('userLogin'));
                console.log(user.id);
-               
+
                const url = `${process.env.REACT_APP_MOCKAPI_URL}/users/${user.id}/shopping-lists`;
 
                const options = {
@@ -45,7 +73,7 @@ function ShoppingListsPage() {
                   .then((response) => response.json())
                   .then((result) => {
                      alert('Barang berhasil ditambahkan');
-
+                     window.location.reload()
 
                   })
                   .catch((error) => console.log(error));
@@ -146,6 +174,42 @@ function ShoppingListsPage() {
                </Form>
             )}
          </Formik>
+         <div>
+            <h1>list item</h1>
+            {allList.map((list) => {
+               return (
+                  <Card key={list.id} style={{maxWidth:"350px"}}>
+                     <CardActionArea>
+                        <CardMedia
+                           component="img"
+                           alt="Contemplative Reptile"
+                           height="140"                          
+                           image={list.imageURL}
+                           title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                           <Typography gutterBottom variant="h5" component="h2">
+                              {list.name}
+          </Typography>
+                           <Typography variant="body2" color="textSecondary" component="p">
+                              {list.description}
+          </Typography>
+                        </CardContent>
+                     </CardActionArea>
+                     <CardActions>
+                        <Button size="small" color="primary">
+                           Share
+        </Button>
+                        <Button size="small" color="primary">
+                           Learn More
+        </Button>
+                     </CardActions>
+                  </Card>
+               )
+            }
+
+            )}
+         </div>
       </div>
    );
 }
